d4b166336c3558af671673fbdb0a8c86e06ea933
Bill Fanselow <bfanselow@gmail.com>
2026-03-15 15:24:56 -0600

    cleanup

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 // Landing
@@ -8,13 +8,15 @@ const Landing = lazy(() => import("./landing/pages/Landing"));
 
 const AppRoutes = () => {
   return (
-    <Routes basename={process.env.PUBLIC_URL}>
-      <Route path="/" element={<Landing />} />
-      <Route
-        path="*"
-        element={<Navigate to={`/${process.env.PUBLIC_URL}/404`} replace />}
-      />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes basename={process.env.PUBLIC_URL}>
+        <Route path="/" element={<Landing />} />
+        <Route
+          path="*"
+          element={<Navigate to={`/${process.env.PUBLIC_URL}/404`} replace />}
+        />
+      </Routes>
+    </Suspense>
   );
 };
 
--- src/landing/pages/Landing.tsx
+++ src/landing/pages/Landing.tsx
@@ -1,17 +1,29 @@
-import LandingLayout from "../components/LandingLayout";
 import { Box, Card, CardMedia } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import React, { useEffect, useState } from "react";
+
+const REDIRECT = "http://134.209.175.8/";
 
 const Landing = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
+  const handleClick = () => {
+    window.location.href = REDIRECT; // navigate the URL
+  };
 
   return (
-    <LandingLayout>
-      <main>
+    <Paper square sx={{ backgroundColor: "#000", height: "100vh" }}>
       <Box
         display="flex"
         justifyContent="center"
         alignItems="center"
       >
-          <Card square={true} sx={{ height: "80vh" }}>
+          <Card square={true}>
                 <CardMedia
                     component="img"
                     height="70%"
@@ -20,9 +32,19 @@ const Landing = () => {
                 />
           </Card>
       </Box>
-      </main>
-    </LandingLayout>
+      <Button
+             sx={{
+              marginTop: 2,
+              opacity: isVisible ? 1: 0,
+              transition: "opacity 20s ease-in-out",
+              }}
+              onClick={handleClick} 
+              variant="contained"
+              size="medium"
+          >
+             Enter
+      </Button>
+    </Paper>
   );
 };
-
 export default Landing;
